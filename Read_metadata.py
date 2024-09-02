059d2d78f28cf2d89f66b5633d7e7d8bf3e54380
Nicolas Martinez <57869789+NicolasLTU@users.noreply.github.com>
2024-09-02 19:24:59 +0200

    Update Read_metadata.py

--- Read_metadata.py
+++ Read_metadata.py
@@ -2,11 +2,9 @@ from PIL import Image
 
 # Path to your 16-bit PNG file
 file_path = r"C:\Users\auroras\.venvMISS2\MISS2\Captured_PNG\2024\03\19\MISS2-20240319-220230.png"
-
-# Open the image
 image = Image.open(file_path)
 
-# Print basic metadata
+# Print metadata
 print(f"Format: {image.format}")
 print(f"Mode: {image.mode}")  # 'I;16' for 16-bit grayscale
 print(f"Size: {image.size}")
@@ -17,5 +15,4 @@ metadata = image.info
 for key, value in metadata.items():
     print(f"{key}: {value}")
 
-# Don't forget to close the image file
 image.close()
